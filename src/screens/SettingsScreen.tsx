f26a62588aae7659522a562ed71c01b4d8f420de
Sergio Andrés Ruiz Vélez <sergio.ruiz@carroya.com>
2022-05-16 16:51:23 -0500

    Uso de UseSafeAreaInsets

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {styles} from '../themes/apptheme';
 
 const SettingsScreen = () => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View>
+    <View
+      style={{
+        ...styles.globalMargin,
+        marginBottom: insets.top + 20,
+      }}>
       <Text>SettingsScreen</Text>
     </View>
   );
