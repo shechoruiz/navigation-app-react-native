83c35201b3ddaef49da4c0556d2bc7d148616164
Sergio Andrés Ruiz Vélez <sergio.ruiz@carroya.com>
2022-05-26 11:20:39 -0500

    Disparar el action del reducer desde otra screen

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useContext} from 'react';
+import {Button, Text, View} from 'react-native';
+import {AuthContext} from '../context/AuthContext';
+import {styles} from '../themes/apptheme';
 
 const ContactsScreen = () => {
+  const {signIn, authState} = useContext(AuthContext);
+
   return (
-    <View>
-      <Text>Contacts Screen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.text}>Contacts Screen</Text>
+      {!authState.isLoggedIn && (
+        <Button title="Iniciar sesión" onPress={signIn} />
+      )}
     </View>
   );
 };
